7b8963b53ccb7435e1c8f965d71cdbf1a603f584
Fred Snyder <safe.sand1626@fastmail.com>
2023-03-10 12:48:20 +0100

    Setup basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+// MUI components
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
 // app styles
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>App</h1>
+			<Container maxWidth="sm">
+				<h1>App</h1>
+				<Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+			</Container>
 		</div>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// components
 import App from './App';
 
 // MUI components
