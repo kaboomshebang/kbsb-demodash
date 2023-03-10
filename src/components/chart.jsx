f11cf08692f005abea4cd4e5f561ac91bfbff172
Fred Snyder <safe.sand1626@fastmail.com>
2023-03-10 11:30:30 +0100

    Responsive

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,19 +1,21 @@
-import { LineChart, Line, CartesianGrid } from 'recharts';
+import { LineChart, Line, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 import './chart.css';
 
-export const RenderLineChart = () => {
-	const data = [
-		{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
-		{ name: 'Page B', uv: 200, pv: 2400, amt: 2200 },
-	];
+const data = [
+	{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+	{ name: 'Page B', uv: 200, pv: 2400, amt: 2200 },
+];
 
+export const RenderLineChart = () => {
 	return (
 		<div className="chart">
-			<LineChart width={400} height={400} data={data}>
-				<Line type="monotone" dataKey="uv" stroke="#111111" />
-				<CartesianGrid stroke="#ccc" />
-			</LineChart>
+			<ResponsiveContainer width="100%" height="80%">
+				<LineChart data={data}>
+					<Line type="monotone" dataKey="uv" stroke="#111111" />
+					<CartesianGrid stroke="#ccc" />
+				</LineChart>
+			</ResponsiveContainer>
 		</div>
 	);
 };
