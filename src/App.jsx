6db393ef0061eb035dc71a816cd327498c836ca4
Fred Snyder <safe.sand1626@fastmail.com>
2023-03-17 23:23:25 +0100

    Update User comp. fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Header } from '@/components/Header';
 import { Chart } from '@/components/charts/Chart';
 
 // widgets
+import { User } from '@/components/widgets/User';
 import { Search } from '@/components/widgets/Search';
 import { Banking } from '@/components/widgets/Banking';
 import { Favorites } from '@/components/widgets/Favorites';
@@ -33,6 +34,9 @@ function App() {
 					</Grid>
 					<Grid xs={8}>
 						<Favorites />
+					</Grid>
+					<Grid xs={8}>
+						<User />
 					</Grid>
 				</Grid>
 			</Container>
--- src/components/widgets/User.jsx
+++ src/components/widgets/User.jsx
@@ -1,8 +1,40 @@
+import { useState, useEffect } from 'react';
+
+// MUI components
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+// RandomUser.me API
+const endPoint = 'https://randomuser.me/api';
+
 export const User = () => {
+	const [user, setUser] = useState(null);
+	const [refetch, setRefetch] = useState(1);
+
+	useEffect(() => {
+		let fetchedUser = false; // fetch API only once
+
+		const fetchData = async () => {
+			const data = await fetch(endPoint);
+			const json = await data.json();
+			!fetchedUser && setUser(json.results[0]);
+		};
+
+		if (user === null) {
+			fetchData().catch(console.error);
+		}
+
+		return () => (fetchedUser = true); // cleanup function
+	}, [refetch]);
+
+	const refetchHandler = () => {
+		setUser(null);
+		setRefetch((prev) => -prev);
+	};
+
+	console.log('RandomUser.me data', user);
+
 	return (
 		<Paper elevation={2}>
 			<Box p={2}>
@@ -11,6 +43,10 @@ export const User = () => {
 				</Typography>
 				<p>https://randomuser.me/api</p>
 				<p>Avatar</p>
+				<p>{user && user.gender}</p>
+				<p>This does not work, because a re-render triggers a statecahnge?</p>
+				<img src={user && user.picture.thumbnail} alt="" />
+				<button onClick={refetchHandler}>Fetch new user</button>
 			</Box>
 		</Paper>
 	);
