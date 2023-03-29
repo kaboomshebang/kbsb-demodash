959bd30b9a204a9a3dd4fa2f5d2b15ed0595b2ef
Fred Snyder <safe.sand1626@fastmail.com>
2023-03-29 22:03:08 +0200

    Refactor (separate router)

--- lambdas/todos/app.py
+++ lambdas/todos/app.py
@@ -1,46 +1,20 @@
 import json
-
 from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from mangum import Mangum
-from dotenv import load_dotenv
-
-# load environment variables
-load_dotenv()
-
-# import routes
-from routes.airtable_post import post_at
-from routes.airtable_list import list_records
+from routes import new_todo, list_todos
 
 app = FastAPI()
+app.include_router(new_todo.router)
+app.include_router(list_todos.router)
 
 
 @app.get("/")
 def root():
     return {
         "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "Hello todo!",
-            }
-        ),
+        "body": json.dumps({"message": "Hello todo!"}),
     }
-
-
-@app.post("/post_at")
-def post_at_handler():
-    event = {"body": json.dumps({"description": "test", "label": "Normal"})}
-    res = post_at(event)
-
-    return res
-
-
-# TODO: refactor functions so that the decorator only works on the list_records function
-#! OR, move the decorator to the function in the routes folder
-@app.get("/post_at")
-def list_records_handler():
-    list_records()
-    return
 
 
 @app.post("/hello")
