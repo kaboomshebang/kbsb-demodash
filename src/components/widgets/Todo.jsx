f845c9e3a017cf1043cee7b6548ed7af6a2d2a6b
Fred Snyder <safe.sand1626@fastmail.com>
2023-04-03 17:15:56 +0200

    Add AddTodoForm elements

--- src/components/widgets/Todo.jsx
+++ src/components/widgets/Todo.jsx
@@ -1,39 +1,58 @@
 import { FormContainer, TextFieldElement } from 'react-hook-form-mui';
+import { SwitchElement } from 'react-hook-form-mui';
+import { SelectElement } from 'react-hook-form-mui';
 
+import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-const TodoForm = () => {
+const AddTodoForm = () => {
+	const submitHandler = (data) => {
+		console.log(data);
+	};
+
 	return (
-		<FormContainer defaultValues={{ name: '' }} onSuccess={(data) => console.log(data)}>
-			<TextFieldElement name="name" label="Name" required />
+		<FormContainer defaultValues={{ label: 'normal' }} onSuccess={submitHandler}>
+			<TextFieldElement name="description" label="Description" required fullWidth />
+			<Box display="flex" gap={2}>
+				<SelectElement
+					margin="normal"
+					name="label"
+					label="Priority"
+					required
+					options={[
+						{
+							id: 'normal',
+							label: 'Normal',
+						},
+						{
+							id: 'prio',
+							label: 'Prio',
+						},
+						{
+							id: 'longterm',
+							label: 'Longterm',
+						},
+					]}
+				/>
+				<SwitchElement margin="normal" name="done" label="Done" />
+			</Box>
+			<Button variant="outlined" type="submit">
+				Submit
+			</Button>
 		</FormContainer>
 	);
 };
 
 export const Todo = () => {
-	const submitHandler = (e) => {
-		e.preventDefault();
-		console.log('Click');
-	};
-
 	return (
 		<Paper elevation={2}>
 			<Box p={2}>
 				<Typography variant="h6" component="h3">
-					Todo
+					Add todo
 				</Typography>
-				<p>Uses JSON placeholder for data</p>
-				<ul>
-					<li>https://jsonplaceholder.typicode.com/todos/1</li>
-					<li>todo item</li>
-				</ul>
-				<form action="" onSubmit={submitHandler}>
-					<label htmlFor="">DEsc</label>
-					<input id="description" name="description" />
-					<button type="submit">Add</button>
-				</form>
+				<AddTodoForm />
 			</Box>
 		</Paper>
 	);
