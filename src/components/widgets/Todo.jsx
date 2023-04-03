996af313c8ae4d34a82e356a2fc419e59fbbae43
Fred Snyder <safe.sand1626@fastmail.com>
2023-04-03 22:35:14 +0200

    Add endpoint fetch logic

--- src/components/widgets/Todo.jsx
+++ src/components/widgets/Todo.jsx
@@ -7,9 +7,25 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+let endpoint;
+if (process.env.NODE_ENV === 'development') {
+	endpoint = 'http://100.111.214.8:8000/new_todo';
+} else if (process.env.NODE_ENV === 'production') {
+	endpoint = 'https://jchv4b5lpu5gea3lkowr5g2bei0tpnsl.lambda-url.eu-central-1.on.aws/new_todo';
+}
+
 const AddTodoForm = () => {
-	const submitHandler = (data) => {
-		console.log(data);
+	const submitHandler = async (data) => {
+		const res = await fetch(endpoint, {
+			method: 'POST',
+			body: JSON.stringify({
+				description: data.description,
+				label: data.label,
+				done: data.done,
+			}),
+		});
+		const json = await res.json().catch(console.error);
+		console.log(json);
 	};
 
 	return (
