a24f3c1ecb6a5c525d18885f9eccc4137a9a1285
Fred Snyder <safe.sand1626@fastmail.com>
2023-04-03 22:35:22 +0200

    Fix CORS issue

--- lambdas/todos/app.py
+++ lambdas/todos/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from mangum import Mangum
 from routes import new_todo, list_todos
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(new_todo.router)
 app.include_router(list_todos.router)
 
