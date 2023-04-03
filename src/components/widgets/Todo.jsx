04ed14b223e15347164138b539ab90cde574fc66
Fred Snyder <safe.sand1626@fastmail.com>
2023-04-03 14:13:06 +0200

    Scaffold Todo form elements

--- src/components/widgets/Todo.jsx
+++ src/components/widgets/Todo.jsx
@@ -1,8 +1,23 @@
+import { FormContainer, TextFieldElement } from 'react-hook-form-mui';
+
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+const TodoForm = () => {
+	return (
+		<FormContainer defaultValues={{ name: '' }} onSuccess={(data) => console.log(data)}>
+			<TextFieldElement name="name" label="Name" required />
+		</FormContainer>
+	);
+};
+
 export const Todo = () => {
+	const submitHandler = (e) => {
+		e.preventDefault();
+		console.log('Click');
+	};
+
 	return (
 		<Paper elevation={2}>
 			<Box p={2}>
@@ -14,6 +29,11 @@ export const Todo = () => {
 					<li>https://jsonplaceholder.typicode.com/todos/1</li>
 					<li>todo item</li>
 				</ul>
+				<form action="" onSubmit={submitHandler}>
+					<label htmlFor="">DEsc</label>
+					<input id="description" name="description" />
+					<button type="submit">Add</button>
+				</form>
 			</Box>
 		</Paper>
 	);
