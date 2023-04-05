034537d9c6f67c6a765006b9d531a66a8ce3c04d
Fred Snyder <safe.sand1626@fastmail.com>
2023-04-05 15:30:21 +0200

    Add description

--- src/components/widgets/GDP.jsx
+++ src/components/widgets/GDP.jsx
@@ -5,6 +5,9 @@ import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 import Link from '@mui/material/Link';
 
+// icons
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+
 import { ApexChart } from '@/components/charts/ApexCharts';
 
 import gdp from '@/data/nld_gdp.json';
@@ -18,14 +21,21 @@ export const GDP = () => {
 						Annual GDP growth - The Netherlands
 					</Typography>
 
-					<Alert severity="info">
-						React ApexCharts. JSON Source:{' '}
-						<Link href="https://observablehq.com/d/41d0060afaf5f92e">
-							Observable Notebook
-						</Link>
-					</Alert>
+					<Alert severity="info">Created with React ApexCharts.</Alert>
 
 					<ApexChart data={gdp} />
+					<Alert severity="warning">
+						Data extracted with an:{' '}
+						<Link
+							href="https://observablehq.com/d/41d0060afaf5f92e"
+							target="_blank"
+							rel="noopener noreferrer"
+							sx={{ display: 'inline-flex', alignItems: 'center', gap: 1 }}
+						>
+							Observable notebook
+							<OpenInNewIcon fontSize="small" />
+						</Link>
+					</Alert>
 				</Stack>
 			</Box>
 		</Paper>
--- src/components/widgets/MotorRacing.jsx
+++ src/components/widgets/MotorRacing.jsx
@@ -5,6 +5,9 @@ import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 import Link from '@mui/material/Link';
 
+// icons
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+
 import { Rechart } from '@/components/charts/Rechart';
 
 import data from '@/data/ergast_mrd.json';
@@ -18,12 +21,28 @@ export const MotorRacing = () => {
 						Formula 1 accidents (per season)
 					</Typography>
 					<Alert severity="info">
-						Created with Recharts. JSON source:{' '}
-						<Link href="https://observablehq.com/d/889d93794bcfd6da">
-							ObservableHQ notebook
+						Created with Recharts. Read more about{' '}
+						<Link
+							href="https://safetydriven.ca/resource/accident-or-collision/"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							the difference between an accident and a collision.
 						</Link>
 					</Alert>
 					<Rechart data={data} />
+					<Alert severity="warning">
+						Data extracted with an:{' '}
+						<Link
+							href="https://observablehq.com/d/889d93794bcfd6da"
+							target="_blank"
+							rel="noopener noreferrer"
+							sx={{ display: 'inline-flex', alignItems: 'center', gap: 1 }}
+						>
+							ObservableHQ notebook
+							<OpenInNewIcon fontSize="small" />
+						</Link>
+					</Alert>
 				</Stack>
 			</Box>
 		</Paper>
